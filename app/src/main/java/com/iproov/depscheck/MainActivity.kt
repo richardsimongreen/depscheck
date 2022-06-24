d7290c57b0c1a1b1e2443ca021819f5937caa7f4
Richard Green <richard.green@iproov.com>
2022-06-24 09:14:53 +0100

    Added usage

--- app/src/main/java/com/iproov/depscheck/MainActivity.kt
+++ app/src/main/java/com/iproov/depscheck/MainActivity.kt
@@ -2,10 +2,12 @@ package com.iproov.depscheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iproov.sdk.IProov
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        IProov.getBuildNumber()
     }
 }
